c6347aa1b73107fb3e7515de8ea23c6e455f4118
Miutour <miutour@MiutourdeMac-mini.local>
2015-07-20 14:40:56 +0800

    Initial Commit

---
+++ MiuTour/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MiuTour
+//
+//  Created by Miutour on 15/7/20.
+//  Copyright (c) 2015年 Miutour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MiuTour/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MiuTour
+//
+//  Created by Miutour on 15/7/20.
+//  Copyright (c) 2015年 Miutour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
